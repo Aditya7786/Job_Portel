2b4afb50c256c3877f05ae4dcdf174b596e9cbcb
aditya7786 <adityagajbhiye8421@gmail.com>
2024-11-08 23:53:46 +0530

    updated Day 2 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Applayout from "./layouts/app.layout";
+import landing from "./pages/landing";
 
+
+const router = createBrowserRouter([
+  {
+    element:<Applayout/>,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      }
+    ]
+  }
+])
 function App() {
-  return <div>Subscribe to my Channel</div>;
+  // return (
+  
+  // );
 }
 
 export default App;
